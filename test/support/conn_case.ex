546922ad41f268df44a11b73a9bc2d95f6aa6ed9
Jefferson Queiroz Venerando <jefferson@venerando.io>
2020-09-06 22:01:01 -0400

    Resolve remaining credo issues

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -17,6 +17,8 @@ defmodule GuildaWeb.ConnCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       # Import conveniences for testing with connections
@@ -32,10 +34,10 @@ defmodule GuildaWeb.ConnCase do
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Guilda.Repo)
+    :ok = Sandbox.checkout(Guilda.Repo)
 
     unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(Guilda.Repo, {:shared, self()})
+      Sandbox.mode(Guilda.Repo, {:shared, self()})
     end
 
     {:ok, conn: Phoenix.ConnTest.build_conn()}
