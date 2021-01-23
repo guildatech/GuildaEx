6ee0095116becf932a19f9a1ac37a8feede15bc5
Jefferson Queiroz Venerando <jefferson@venerando.io>
2021-01-23 12:42:12 -0500

    complete first iteration of podcasts

--- lib/guilda_web/live/episode_live/episode_component.ex
+++ lib/guilda_web/live/episode_live/episode_component.ex
@@ -7,11 +7,15 @@ defmodule GuildaWeb.PodcastEpisodeLive.EpisodeComponent do
   def render(assigns) do
     ~L"""
     <tr id="<% @id %>">
-      <td class="Table__td"><%= @episode.slug %></td>
+      <td class="Table__td">
+        <img src="<%= @episode.cover_url %>" width="150" alt="<%= gettext("Capa do episódio %{episode_name}", episode_name: @episode.title) %>">
+      </td>
+      <td class="Table__td">
+        <%= link @episode.file_name, to: @episode.file_url %>
+      </td>
       <td class="Table__td"><%= @episode.title %></td>
-      <td class="Table__td"><%= @episode.cover_url %></td>
-      <td class="Table__td"><%= @episode.file_url %></td>
       <td align="right" class="Table__td"><%= @episode.length %></td>
+      <td align="right" class="Table__td"><%= @episode.play_count %></td>
       <td align="right" class="Table__td">
         <%= live_patch gettext("Editar"), to: Routes.podcast_episode_index_path(@socket, :edit, @episode) %> | <button type="button" phx-click="delete" phx-target="<%= @myself %>" phx-value-id="<%= @episode.id %>" data-confirm="Tem certeza?"><%= gettext("Excluir") %></button>
       </td>
