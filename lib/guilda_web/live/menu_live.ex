7ae50425e03a24194844a9f765d1ed842505fbc5
Jefferson Queiroz Venerando <jefferson@venerando.io>
2021-01-30 13:08:23 -0500

    fix mobile menu

--- lib/guilda_web/live/menu_live.ex
+++ lib/guilda_web/live/menu_live.ex
@@ -26,7 +26,7 @@ defmodule GuildaWeb.MenuLive do
   end
 
   defp active_class(:mobile) do
-    "block pl-3 pr-4 py-2 border-l-4 border-yellow-500 text-base font-medium text-yellow-700 bg-yellow-50 focus:outline-none focus:text-yellow-800 focus:bg-yellow-100 focus:border-yellow-700 transition duration-150 ease-in-out"
+    "block pl-3 pr-4 py-2 border-l-4 text-base font-medium text-gray-600 bg-yellow-50 border-yellow-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
   end
 
   defp inactive_class(:main) do
@@ -34,6 +34,6 @@ defmodule GuildaWeb.MenuLive do
   end
 
   defp inactive_class(:mobile) do
-    "inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+    "block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
   end
 end
